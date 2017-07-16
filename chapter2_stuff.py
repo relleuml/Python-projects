30983a91ee33084849deffe4f325c0a6319fc30a
relleuml <relleuml@gmail.com>
2017-07-16 03:52:34 -0500

    Chapter 2 file...

---
+++ chapter2_stuff.py
@@ -0,0 +1,12 @@
+# Leonard Mueller - 5/24/16
+# Python Crash Course - pg. 32
+age = 23
+message = "Happy " + str(age) + "rd Birthday!"
+print(message)
+
+print(5 + 3)
+print(5 + 3/1)
+
+favorite_number = 13
+message = "My favorite number is " + str(favorite_number) + " because I was born on the 13th!"
+print(message)
